3e475cbc75a61350b381eaf0f03134d41f74dec2
Alifseen <alifseen@gmail.com>
2024-11-15 18:15:51 +0500

    Created form model to fetch data by creating a model class in a forms.py and data variables in view function by instantiating that class

--- job_application/views.py
+++ job_application/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["fname"]
+            last_name = form.cleaned_data["lname"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+
     return render(request, "index.html")  ## This looks for a folder called "templates" and then searchs for the file name inside it.
