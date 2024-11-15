b00cf32e11559a7b566c91ae04d6cdd83e2d05e2
Alifseen <alifseen@gmail.com>
2024-11-15 16:12:41 +0500

    Created a Database Table Model (fixed typo when connecting app to project in settings.py)

--- job_application/models.py
+++ job_application/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+## Create a table bluebrint with columns
+class Forms(models.Model):
+    fname = models.CharField(max_length=80)
+    lname = models.CharField(max_length=80)
+    email = models.EmailField()
+    date = models.DateField()
+    occupation = models.CharField(max_length=80)
+
+    ## Define a string to return the data when printed with an instance
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'job-application', ## Added App directory here
+    'job_application', ## Added App directory here
 ]
 
 MIDDLEWARE = [
