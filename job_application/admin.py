5c24d7b7ea741755352acd0e760d87f6117a4693
Alifseen <alifseen@gmail.com>
2024-11-15 19:43:10 +0500

    Customized the admin panel table for forms

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Forms
 
+class FormsAdmin(admin.ModelAdmin):
+    list_display = ("fname", "lname", "email", "occupation")  ## Displays these column name values on the table
+    search_fields = ("fname", "lname", "email")  ## Adds a search bar
+    list_filter = ("date", "occupation")  ## Adds a filter
+    ordering = ("-fname", )  ## Descending order
+    readonly_fields = ("occupation", )  ## amkes the field uneditable
 
-
-admin.site.register(Forms)
+admin.site.register(Forms, FormsAdmin)
